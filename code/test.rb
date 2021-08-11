468394a6cae8cd83f13e7c332e0fe590ba9b9300
tateishishozo <tateishi.shozo@gmail.com>
2021-08-11 21:04:57 +0900

    {first}

---
+++ code/test.rb
@@ -0,0 +1,15 @@
+puts hellow
+
+array = [x,...,y]
+k = 0
+i = array.length
+
+while k >= |i| do
+  for k >= |i| then
+    if array[k] > array[k+1] then
+      array[k] = W
+      array[k+1] = array[k]
+      W = array[k+1]
+  end
+  i = i - 1s
+end
